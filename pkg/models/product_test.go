9c16a89b18252fe46e03be2aaf6f5a4ae69eef46
sleepynut <nut.pa@kbtg.tech>
2024-10-20 16:41:39 +0700

    using table-driven for product_test.go

--- pkg/models/product_test.go
+++ pkg/models/product_test.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"reflect"
 	"runtime/debug"
 	"testing"
@@ -12,36 +13,56 @@ func TestRawToJson(t *testing.T) {
 			t.Errorf("unexpected panic: %v", string(debug.Stack()))
 		}
 	}()
-
 	depJson := `{
-"department": [
-	"Mens",
-	"Shoes",
-	"Running Shoes"
-]}`
-	// 	depJson := `[
-	// 	"Mens",
-	// 	"Shoes",
-	// 	"Running Shoes"
-	// ]`
+		"department": [
+			"Mens",
+			"Shoes",
+			"Running Shoes"
+		]}`
 
-	p := Product{
-		Id:             1,
-		Sku:            "NIKE-BLCK-42-M-1",
-		Name:           "stan smith sneaker",
-		ImageUrl:       "Stan_Smith_Lux_Shoes_Black_IH2450_01_standard.avif",
-		DepartmentRaw:  []uint8([]byte(depJson)),
-		DepartmentJson: Deps{Department: []string{}},
+	cases := []struct {
+		name    string
+		field   string
+		rawJson string
+		p       Product
+		err     error
+	}{
+		{
+			name:    "happycase: field - Department",
+			field:   "Department",
+			rawJson: `{"department":["Mens","Shoes","Running Shoes"]}`,
+			p: Product{
+				Id:             1,
+				Sku:            "NIKE-BLCK-42-M-1",
+				Name:           "stan smith sneaker",
+				ImageUrl:       "Stan_Smith_Lux_Shoes_Black_IH2450_01_standard.avif",
+				DepartmentRaw:  []uint8([]byte(depJson)),
+				DepartmentJson: Deps{Department: []string{}},
+			},
+			err: nil,
+		},
 	}
-	err := p.RawToJson("Department")
-	if err != nil {
-		t.Errorf("not expecte error, but got %v", err)
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			err := c.p.RawToJson(c.field)
+			if c.err == nil && err != nil {
+				t.Errorf("not expecte error, but got %v", err)
+			}
+
+			if reflect.ValueOf(c.p.DepartmentJson).IsZero() {
+				t.Errorf("expect value, but got nil")
+			}
+
+			b, err := json.Marshal(reflect.ValueOf(c.p).FieldByName(c.field + "Json").Interface())
+			if err != nil {
+				t.Errorf("not expecte error, but got %v", err)
+			}
+
+			if string(b) != c.rawJson {
+				t.Errorf("Marshal mismatch] expected %s\n,but got %s\n", c.rawJson, b)
+			}
+
+		})
 	}
-
-	if reflect.ValueOf(p.DepartmentJson).IsZero() {
-		t.Errorf("expect value, but got nil")
-	}
-
-	t.Logf("JSON value: %v", p.DepartmentJson)
-
 }
